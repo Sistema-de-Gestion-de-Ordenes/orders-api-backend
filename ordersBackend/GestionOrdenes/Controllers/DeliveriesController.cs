c254196c82fda1f14f4b3addfb4e78ee24a3c367
DorianCalderon <dorian.calderon.montoya@est.una.ac.cr>
2026-05-28 17:05:40 -0600

    AllowAnonymous to Authorize

--- ordersBackend/GestionOrdenes/Controllers/DeliveriesController.cs
+++ ordersBackend/GestionOrdenes/Controllers/DeliveriesController.cs
@@ -8,7 +8,7 @@ namespace OrderManagement.Controllers;
 
 [ApiController]
 [Route("deliveries")]
-[AllowAnonymous]
+[Authorize]
 public class DeliveriesController : ControllerBase
 {
     private readonly IDeliveryService _deliveryService;
